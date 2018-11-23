4e16888b29f3ac1c13c8e2bfb3150e8a46189b07
zhengda <zhengda@sztozed.net>
2018-11-23 10:47:18 +0800

    uboot: 设置串口波特率为115200

--- package/boot/openwrt-uboot-mt7628/src/include/configs/rt2880.h
+++ package/boot/openwrt-uboot-mt7628/src/include/configs/rt2880.h
@@ -94,7 +94,7 @@ extern unsigned int  CFG_BLOCKSIZE;
 
 #define CONFIG_BOOTDELAY	5	/* autoboot after 5 seconds	*/
 
-#define CONFIG_BAUDRATE		57600
+#define CONFIG_BAUDRATE		115200
 
 #define CONFIG_SERVERIP 10.10.10.3
 #define CONFIG_IPADDR 10.10.10.123
