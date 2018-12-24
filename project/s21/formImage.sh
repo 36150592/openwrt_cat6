58da28b9af76ade914475bdb64cc6383561a682f
zhengda <zhengda@sztozed.net>
2018-12-24 10:37:41 +0800

    s21: 增加合bin文件操作.

--- project/s21/formImage.sh
+++ project/s21/formImage.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 
 echo "topdir=" $TOPDIR
+echo "project=" $project
+
+TZ_SOFTWARE_VERSION=0.01
+
+## target
+cd $TOPDIR
+
+mkdir -p image/${project}/update
+
+cp -f project/s21/bin/factory.bin image/${project}/factory.bin
+cp -f project/s21/bin/old-all.bin image/${project}/old-all.bin
+
+cp -f bin/ramips/openwrt-ramips-mt7621-sxx-u-boot.bin image/${project}/new_uboot.bin
+cp -f bin/ramips/openwrt-ramips-mt7621-zbt-wg2626-squashfs-sysupgrade.bin image/${project}/new_firmware-kernel-fs.bin
+cp -f project/s21/bin/factory.bin image/${project}/
+cp -f project/s21/bin/old-all.bin image/${project}/
+
+#new uboot new firmware
+rm -f image/${project}/new_firmware_factory.bin
+tr '\000' '\377' < /dev/zero | dd of=image/${project}/new_firmware_factory.bin bs=1024 count=16384 2>/dev/null
+dd if=image/${project}/new_uboot.bin of=image/${project}/new_firmware_factory.bin conv=notrunc 2>/dev/null
+dd if=image/${project}/factory.bin of=image/${project}/new_firmware_factory.bin conv=notrunc bs=64k seek=4 2>/dev/null
+dd if=image/${project}/new_firmware-kernel-fs.bin of=image/${project}/new_firmware_factory.bin conv=notrunc bs=64k seek=5 2>/dev/null
+
+#old uboot new firmware
+cp -f image/${project}/old-all.bin image/${project}/olduboot_newfirmware.bin
+tr '\000' '\377' < /dev/zero | dd of=imag/${project}/emptyfirmware.bin bs=1024 count=6144 2>/dev/null
+dd if=image/${project}/emptyfirmware.bin of=image/${project}/olduboot_newfirmware.bin conv=notrunc bs=64k seek=5 2>/dev/null
+dd if=image/${project}/new_firmware-kernel-fs.bin of=image/7621_cat6/olduboot_newfirmware.bin conv=notrunc bs=64k seek=5 2>/dev/null
+
+
