9bedfd37e1e77459359efcf9d5f4fabe9172e275
yuanzhaosky <yuanzhaosky@163.com>
2019-03-28 13:50:36 +0800

    添加挂侧脚本

--- project/s21/rootfs/etc/tozed/tz_test/soft_continue_test.sh
+++ project/s21/rootfs/etc/tozed/tz_test/soft_continue_test.sh
@@ -71,25 +71,25 @@ check_br()
 	fi
 }
 
+reboot_num=`cat /root/reboot_num`
 
-
-sleep 60
-
-if [ ! -f /etc/reboot_count ]; then
-        echo 0 > /etc/reboot_count
+if [ $reboot_num"" == "" ];then
+	reboot_num=0
 fi
 
-reboot_num=`cat /etc/reboot_count`
+reboot_num=$(($reboot_num +1))
+echo $reboot_num > /root/reboot_num
 
-wait_num=0
+sleep 80
+
 
 while [ 1 ]; 
 do
 	restore_all_status
 
-        check_wifi_24g
-        check_wifi_58g
-        check_br
+    check_wifi_24g
+    check_wifi_58g
+    check_br
 
     ping -w 5 www.baidu.com
     status_4g=$?
@@ -99,28 +99,23 @@ do
 	echo "status_br_wifi=" ${status_br_wifi}
 	echo "status_4g=" ${status_4g}
 
-	if [ "${status_58g_wifi}" = "ok" -a "${status_24g_wifi}" = "ok" -a "$status_br_wifi" = "ok"  -a $status_4g"" == "0" ]; then
-		last_reboot_num=`expr ${reboot_num} + 1`
-		echo ${last_reboot_num} > /etc/reboot_count
+	if [ $status_58g_wifi"" != "ok" -o $status_24g_wifi"" != "ok" -o $status_br_wifi"" != "ok" -o $status_4g"" != "0" ];then
 
-		if [ $wait_num -gt 3 ];then
-			echo "TIMEOUT" >> /etc/reboot_timeout
+		if [ ! -f /root/record.${reboot_num}.log ];then
+			iwconfig >> /root/record.${reboot_num}.log
+			brctl show >> /root/record.${reboot_num}.log
+			logread >> /root/record.${reboot_num}.log
+			echo "---------------------------------------------------------------------" >> /root/record.${reboot_num}.log
 		fi
 
-		sleep 10
-		reboot
 	fi
 
-	echo "wait_num=" ${wait_num}
+	cat /proc/meminfo | grep MemFree >> /root/record.${reboot_num}.continue
+	lsof | wc -l >> /root/record.${reboot_num}.continue
 
-	if [ "${wait_num}" = "3" ]; then
-		iwconfig > /etc/record.${reboot_num}.log
-		brctl show >> /etc/record.${reboot_num}.log
-		logread >> /etc/record.${reboot_num}.log
-	fi
+	sleep 30
+	cat /proc/uptime | awk '{print $1}' > /root/record.${reboot_num}.time
 
-	wait_num=`expr ${wait_num} + 1`
-	sleep 10
 done
 
 
